6d094e2f9278cdf48f6120bb4a6df40230e6d87c
Suraj Kr Das <suraj.kr.das@ibm.com>
2026-04-28 10:36:44 +0530

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return "Hello from Dockerized Flask!"
+@app.route("/status")
+def status():
+    return jsonify({"status": "running", "container": "flask-app"})
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {name}!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
